9ae091c22065c040fae1443b5d6ca71a2c7d0795
Fernando Contreras <fercreek@gmail.com>
2018-03-04 03:33:47 -0600

    Trying to modify the data

--- drugblock.js
+++ drugblock.js
@@ -1,69 +1,76 @@
 const SHA256 = require("crypto-js/sha256");
 
 class Block {
-    constructor(index, timestamp, data, previousHash = '') {
-        this.index = index;
-        this.previousHash = previousHash;
-        this.timestamp = timestamp;
-        this.data = data;
-        this.hash = this.calculateHash();
-        this.nonce = 0;
+  constructor(index, timestamp, data, previousHash = "") {
+    this.index = index;
+    this.previousHash = previousHash;
+    this.timestamp = timestamp;
+    this.data = data;
+    this.hash = this.calculateHash();
+    this.nonce = 0;
+  }
+
+  calculateHash() {
+    return SHA256(
+      this.index +
+        this.previousHash +
+        this.timestamp +
+        JSON.stringify(this.data) +
+        this.nonce
+    ).toString();
+  }
+
+  mineBlock(difficulty) {
+    while (
+      this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")
+    ) {
+      this.nonce++;
+      this.hash = this.calculateHash();
     }
 
-    calculateHash() {
-        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
-    }
-
-    mineBlock(difficulty) {
-        while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
-            this.nonce++;
-            this.hash = this.calculateHash();
-        }
-
-        console.log("BLOCK MINED: " + this.hash);
-    }
+    console.log("BLOCK MINED: " + this.hash);
+  }
 }
 
+class Blockchain {
+  constructor() {
+    this.chain = [this.createGenesisBlock()];
+    this.difficulty = 2;
+  }
 
-class Blockchain{
-    constructor() {
-        this.chain = [this.createGenesisBlock()];
-        this.difficulty = 2;
+  createGenesisBlock() {
+    return new Block(0, "01/01/2017", "Genesis block", "0");
+  }
+
+  getLatestBlock() {
+    return this.chain[this.chain.length - 1];
+  }
+
+  addBlock(newBlock) {
+    newBlock.previousHash = this.getLatestBlock().hash;
+    newBlock.mineBlock(this.difficulty);
+    this.chain.push(newBlock);
+  }
+
+  isChainValid() {
+    for (let i = 1; i < this.chain.length; i++) {
+      const currentBlock = this.chain[i];
+      const previousBlock = this.chain[i - 1];
+
+      if (currentBlock.hash !== currentBlock.calculateHash()) {
+        return false;
+      }
+
+      if (currentBlock.previousHash !== previousBlock.hash) {
+        return false;
+      }
     }
 
-    createGenesisBlock() {
-        return new Block(0, "01/01/2017", "Genesis block", "0");
-    }
-
-    getLatestBlock() {
-        return this.chain[this.chain.length - 1];
-    }
-
-    addBlock(newBlock) {
-        newBlock.previousHash = this.getLatestBlock().hash;
-        newBlock.mineBlock(this.difficulty);
-        this.chain.push(newBlock);
-    }
-
-    isChainValid() {
-        for (let i = 1; i < this.chain.length; i++){
-            const currentBlock = this.chain[i];
-            const previousBlock = this.chain[i - 1];
-
-            if (currentBlock.hash !== currentBlock.calculateHash()) {
-                return false;
-            }
-
-            if (currentBlock.previousHash !== previousBlock.hash) {
-                return false;
-            }
-        }
-
-        return true;
-    }
+    return true;
+  }
 }
 
 module.exports = {
-    Block: Block,
-    Blockchain: Blockchain
-}
+  Block: Block,
+  Blockchain: Blockchain
+};
--- health_record.js
+++ health_record.js
@@ -1,5 +1,19 @@
-const fetch = require('node-fetch');
+const DrugBlock = require("./drugblock");
+const fetch = require("node-fetch");
 
-fetch('http://localhost:3000/patients')
+let record = new DrugBlock.Blockchain();
+
+fetch("http://localhost:3000/patients/1")
   .then(response => response.json())
-  .then(json => console.log(json))
+  .then(json => {
+    console.log(json);
+    record.addBlock(new DrugBlock.Block(1, new Date(), json ));
+    console.log('Is valid?', record.isChainValid())
+
+    record.addBlock(new DrugBlock.Block(2, new Date(), json));
+    console.log('Is valid?', record.isChainValid())
+
+    record.chain[1].data = {random_data: "FAKE DATA"};
+    console.log('Is valid?', record.isChainValid())
+
+  });
